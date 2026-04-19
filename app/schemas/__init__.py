a39a8527ea68f588943577b95f85559ed41b8157
Matthew Belanger <msb64@njit.edu>
2026-04-19 11:09:01 -0400

    User schema

---
+++ app/schemas/__init__.py
@@ -0,0 +1,41 @@
+# TODO: Add the rest of these schemas in once they are defined
+
+from app.schemas.user import (
+    UserBase,
+    UserCreate,
+    UserResponse,
+    UserLogin,
+    UserUpdate,
+    PasswordUpdate,
+)
+
+from app.schemas.token import (
+    Token,
+    # TokenData,
+    # TokenResponse,
+)
+
+# from app.schemas.calculation import (
+#     CalculationType,
+#     CalculationBase,
+#     CalculationCreate,
+#     CalculationUpdate,
+#     CalculationResponse,
+# )
+
+__all__ = [
+    'UserBase',
+    'UserCreate',
+    'UserResponse',
+    'UserLogin',
+    'UserUpdate',
+    'PasswordUpdate',
+    'Token',
+    # 'TokenData',
+    # 'TokenResponse',
+    # 'CalculationType',
+    # 'CalculationBase',
+    # 'CalculationCreate',
+    # 'CalculationUpdate',
+    # 'CalculationResponse',
+]
